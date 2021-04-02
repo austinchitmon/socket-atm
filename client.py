3839dc61230c19893e374c3f33543fc2b5cfb585
austinchitmon <austinchitmon@gmail.com>
2021-04-01 19:47:21 -0500

    Finish main implementation

--- client.py
+++ client.py
@@ -3,11 +3,11 @@ import socket
 # CONSTANTS
 running = True
 OPTION_MESSAGE = "\nEnter the number of your desired ATM Operation:\n" \
-                "1.\tWithdraw from my bank account\n" \
-                "2.\tDeposit into my bank account\n" \
-                "3.\tView my balance\n" \
-                "4.\tClose ATM\n" \
-                "Input: "
+                 "1.\tWithdraw from my bank account\n" \
+                 "2.\tDeposit into my bank account\n" \
+                 "3.\tView my balance\n" \
+                 "4.\tClose ATM\n" \
+                 "Input: "
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
@@ -15,27 +15,60 @@ MESSAGE = "Hello, World!"
 WITHDRAW_MSG = 'withdraw_'
 DEPOSIT_MSG = 'deposit_'
 VIEW_MSG = 'view'
+CLOSE_MSG = 'close'
 
 # CONNECTION SETUP
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
 
+
 def listen_for_server_response():
     data = s.recv(BUFFER_SIZE)
     return data.decode()
 
+
 def handle_response(response):
-    print(response)
+    print('\n%s' % response)
+
 
 def print_invalid_option_selection():
-    print('ERROR: Please enter a number 1-4\n')
+    print('\nERROR: Please enter a number 1-4')
 
-def validate_float_or_int(number):
+
+def print_invalid_monetary_amount():
+    print('\nERROR: Please enter a positive whole dollar amount or a positive amount with two decimal places.')
+
+
+def validate_positive_float_or_int(number):
     try:
-        float(number)
-        return True
+        # check if int
+        int(number)
+        if validate_int(number):
+            # means number entered is an int
+            number = int(number)
+            # check it is greater than 0
+            if number > 0:
+                return True
+            else:
+                # less than 0 int, throw error
+                print_invalid_monetary_amount()
+                return False
     except ValueError:
-        return validate_int(number)
+        # value entered not an int
+        try:
+            # try and convert to float
+            float(number)
+            # only allow positive floats with two decimal places
+            if float(number) > 0 and len(number.rsplit('.')[-1]) == 2:
+                return True
+            else:
+                # means entered float is negative or does not have two decimal places
+                print_invalid_monetary_amount()
+                return False
+        # value entered not a float or int
+        except ValueError:
+            print_invalid_monetary_amount()
+            return False
 
 
 def validate_int(number):
@@ -45,37 +78,55 @@ def validate_int(number):
     except ValueError:
         return False
 
+
 def option_selected_is_invalid(number):
+    # must be int inclusively between 1 and 4
     if validate_int(number) and 1 <= int(number) <= 4:
         return False
     else:
         print_invalid_option_selection()
         return True
 
+
 def send_message(message):
     bytes = message.encode()
     s.send(bytes)
 
+
 def withdraw():
     withdrawAmt = input('Enter an amount to withdraw: $')
-    if validate_float_or_int(withdrawAmt):
+    if validate_positive_float_or_int(withdrawAmt):
         send_message(WITHDRAW_MSG + withdrawAmt)
         # wait for response from server
         responseMsg = listen_for_server_response()
         handle_response(responseMsg)
     else:
-        print('Please enter a valid numerical amount to withdraw')
+        print('Please try again.')
+
 
 def deposit():
-    send_message(DEPOSIT_MSG)
+    depositAmt = input('Enter an amount to deposit: $')
+    if validate_positive_float_or_int(depositAmt):
+        send_message(DEPOSIT_MSG + depositAmt)
+        # wait for response from server
+        responseMessage = listen_for_server_response()
+        handle_response(responseMessage)
+    else:
+        print('Please try again.')
 
 
 def view_balance():
     send_message(VIEW_MSG)
+    # wait for response from server
+    responseMsg = listen_for_server_response()
+    handle_response(responseMsg)
+
 
 def close():
     print('Closing ATM...')
+    send_message(CLOSE_MSG)
     exit()
+
 
 def option_selector(number):
     if number == 1:
--- server.py
+++ server.py
@@ -4,43 +4,109 @@ import socket
 TCP_IP = '127.0.0.1'
 TCP_PORT = 5005
 BUFFER_SIZE = 1024
-WITHDRAW_MSG = 'withdraw_'
-DEPOSIT_MSG = 'deposit_'
+WITHDRAW_MSG = 'withdraw'
+DEPOSIT_MSG = 'deposit'
 VIEW_MSG = 'view'
-BALANCE = 1000
+CLOSE_MSG = 'close'
+BALANCE = 100.00
+s = None
+conn = None
+addr = None
 
+def first_time_setup():
+    global s
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((TCP_IP, TCP_PORT))
 
-# CONNECTION SETUP
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(1)
-print('Listening for client...')
-conn, addr = s.accept()
+def listen_for_client():
+    global s
+    global conn
+    global addr
+    s.listen(1)
+    print('Listening for client...')
+    conn, addr = s.accept()
 
 
 
 def send_response(responseMsg):
     conn.send(responseMsg.encode())
 
+
+def parse_message(message):
+    return message.split('_')
+
+
+def able_to_withdraw(amount):
+    global BALANCE
+    print('LOG: Doing withdraw check with $%.2f - $%.2f' % (BALANCE, amount))
+    val = BALANCE >= amount
+    print('LOG: Able to withdraw?: %r' % val)
+    return val
+
+
+def do_withdraw(amount):
+    global BALANCE
+    BALANCE = BALANCE - amount
+
+
+def withdraw_container(amount):
+    global BALANCE
+    amount = float(amount)
+    if able_to_withdraw(amount):
+        do_withdraw(amount)
+        print('LOG: New balance: $%.2f' % BALANCE)
+        send_response('Successfully withdrew $%.2f' % amount)
+    else:
+        print('LOG: Invalid balance to withdraw')
+        send_response('ERROR: Not enough balance to withdraw.')
+
+
+def do_deposit(amount):
+    global BALANCE
+    print('LOG: Doing deposit: $%.2f + $%.2f' % (BALANCE, amount))
+    BALANCE = BALANCE + amount
+    print('LOG: New balance after deposit: $%.2f' % BALANCE)
+
+
+def deposit_container(amount):
+    global BALANCE
+    amount = float(amount)
+    do_deposit(amount)
+    send_response('Successfully deposited $%.2f' % amount)
+
+
+def view_balance_container():
+    global BALANCE
+    print('LOG: Current Balance: $%.2f' % BALANCE)
+    send_response('Current Balance: $%.2f' % BALANCE)
+
+
 def handle_message(message):
-    if message == WITHDRAW_MSG:
-        send_response('withdrawing...')
-    elif message == DEPOSIT_MSG:
-        send_response('depositing...')
-    elif message == VIEW_MSG:
-        send_response('viewing...')
+    messageList = parse_message(message)
+    if messageList[0] == WITHDRAW_MSG:
+        withdraw_container(messageList[1])
+    elif messageList[0] == DEPOSIT_MSG:
+        deposit_container(messageList[1])
+    elif messageList[0] == VIEW_MSG:
+        view_balance_container()
+    elif messageList[0] == CLOSE_MSG:
+        print('LOG: Current connection closed by client.')
+
 
 def on_connection_established():
     print('Connection established with ', addr)
+    # while connection open, listen for data
     while True:
         data = conn.recv(BUFFER_SIZE)
         if not data:
             break
-        print('received:', data.decode())
+        print('--RECEIVED:', data.decode())
         handle_message(data.decode())
-
-
+        print('--OPERATION COMPLETE')
 
 
 if __name__ == '__main__':
-    on_connection_established()
+    first_time_setup()
+    while True:
+        listen_for_client()
+        on_connection_established()
